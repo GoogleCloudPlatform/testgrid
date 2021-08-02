1067d341215073c000a2b6d68724686dc9e6791a
yairnee <yairnee@google.com>
2021-08-02 11:34:41 +0000

    TestGrid updater - on dynamic email list, solve casting []string issue
    When reading list of strings from json go recognize it as a list of interfaces, which cause casting issues.
    Added to metadata function that get list of strings if the type is list of strings or list of interfaces that represent list of strings, and return if the key is exist.

--- metadata/job.go
+++ metadata/job.go
@@ -131,7 +131,9 @@ func (m Metadata) Strings() map[string]string {
 	return bm
 }
 
-// GetListOfStrings get list of strings if exist, and true if they key is present.
+/* GetListOfStrings get list of strings if exist, and true if they key is present.
+If value is list of strings we return it as is.
+If value is list of interfaces, we try convert it into list of strings, if fail we return an empty list */
 func (m Metadata) GetListOfStrings(name string) ([]string, bool) {
 	if v, ok := m[name]; !ok {
 		return []string{}, false
