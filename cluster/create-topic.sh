f0fbebe4808ec321b6742bf3f58035d0ffe31024
Erick Fejta <fejta@google.com>
2021-08-11 22:00:41 -0700

    PubSub bootstrapping scripts

---
+++ cluster/create-topic.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# Copyright 2021 The TestGrid Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -o nounset
+set -o errexit
+
+# See gsutil notification --help
+
+args=()
+topic=
+
+while getopts "e:t:p:b:" flag; do
+    case "$flag" in
+        e) args+=(-e "$OPTARG");;
+        t) topic=$OPTARG;;
+        p) args+=(-p "$OPTARG");;
+    esac
+done
+
+shift $((OPTIND -1))
+
+if [[ -z "$topic" || $# == 0 ]]; then
+    echo "Usage: $(basename "$0") [-e EVENT] [-p PREFIX] <-t TOPIC> <BUCKET ...>" >&2
+    echo >&2
+    echo "  -e EVENT: OBJECT_FINALIZE|OBJECT_METADATA_UPDATE|OBJECT_DELETE|OBJECT_ARCHIVE (repeatable)" >&2
+    echo "  -t TOPIC: foo or projects/proj/topics/foo" >&2
+    echo >&2
+    echo "   More info: gsutil notification --help" >&2
+    exit 1
+fi
+
+log() {
+    (
+        set -o xtrace
+        "$@"
+    )
+}
+
+for bucket in "$@"; do
+    existing=( $(gsutil notification list "$bucket" 2>/dev/null | grep -B 1 "$topic" | grep notificationConfigs || true) )
+    if [[ ${#existing[@]} -gt 0 ]]; then
+        log gsutil notification delete "${existing[@]}"
+    fi
+    log gsutil notification create -f json -t "$topic" "${args[@]}" "$bucket"
+done
---
+++ cluster/setup-pubsub.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+# Copyright 2021 The TestGrid Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -o nounset
+set -o errexit
+
+buckets=(
+    gs://oss-prow
+    gs://istio-prow
+)
+
+topic=projects/k8s-testgrid/topics/prow-updates
+
+dir=$(dirname "$0")
+list=$dir/list-gcs-prefixes.sh
+create_topic=$dir/create-topic.sh
+create_sub=$dir/create-subscription.sh
+
+
+log() {
+    (
+        set -o xtrace
+        "$@"
+    )
+}
+
+apply-subscription() {
+    log "$create_sub" -c -t "$topic" -p k8s-testgrid testgrid
+}
+
+apply-topic() {
+    log "$create_topic" -t "$topic" -p logs/ -p pr-logs/ "${buckets[@]}"
+}
+
+do-list() {
+    "$list" gs://k8s-testgrid-canary/config
+}
+
+something=
+while getopts "lst" flag; do
+    case "$flag" in
+        s)
+            apply-subscription
+            something=yes
+            ;;
+        t)
+            apply-topic
+            something=yes
+            ;;
+        l)
+            do-list
+            something=yes
+            ;;
+    esac
+done
+
+if [[ -z "$something" ]]; then
+    echo "Usage: $(basename "$0") [-u] [-s] [-l]" >&2
+    echo >&2
+    echo "  -l: list buckets in use" >&2
+    echo "  -t: configure topics for ${buckets[@]}" >&2
+    echo "  -s: configure subscriptions" >&2
+    exit 1
+fi
