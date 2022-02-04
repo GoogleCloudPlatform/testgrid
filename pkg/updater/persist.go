8f262616dc45c097b9826b9146a7a1a7fbeb84bd
Erick Fejta <fejta@google.com>
2022-02-04 13:49:50 -0800

    Remove duplicate package comment

--- pkg/updater/persist.go
+++ pkg/updater/persist.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package updater reads the latest test results and saves updated state.
 package updater
 
 import (
