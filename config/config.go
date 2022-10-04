8063b2f6c66a8f4aa6e99ee6af6e9049366c0e7f
Sultan Duisenbay <sduisenbay@google.com>
2022-10-04 15:48:07 +0000

    More detailed error message for max_acceptable_flakines allowed values.

--- config/config.go
+++ config/config.go
@@ -415,7 +415,7 @@ func validateDashboardTab(dt *configpb.DashboardTab) error {
 
 	// Max acceptable flakiness parameter should be valid (between 0.0 and 100.0 - both inclusive).
 	if max_acceptable_flakiness := dt.GetStatusCustomizationOptions().GetMaxAcceptableFlakiness(); max_acceptable_flakiness < 0 || max_acceptable_flakiness > 100 {
-		mErr = multierror.Append(mErr, errors.New("invalid value provided for max_acceptable_flakiness"))
+		mErr = multierror.Append(mErr, errors.New("invalid value provided for max_acceptable_flakiness (should be between 0.0 and 100.0)"))
 	}
 
 	return mErr
