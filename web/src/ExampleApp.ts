dd0c2829ab5085467f91a44deb41252566774880
Sean Chase <slchase@google.com>
2022-11-10 16:50:42 -0800

    Add material shininess

--- web/src/ExampleApp.ts
+++ web/src/ExampleApp.ts
@@ -3,35 +3,38 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { map } from 'lit/directives/map.js';
 import { ListDashboardResponse } from './pb/api/v1/data.js';
+import '@material/mwc-button';
+import '@material/mwc-list';
 
 @customElement('example-app')
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class ExampleApp extends LitElement {
   @property({ type: String }) title = 'My app';
 
-  @property({ type: Array<String> }) dashboards = ['foo'];
+  @property({ type: Array<string> }) dashboards: Array<string> = [];
 
   render() {
     return html`
-      <ol>
-        ${map(this.dashboards, dash => html`<li>${dash}</li>`)}
-      </ol>
-      <button @click="${this.getDashboards}">Call API</button>
+      <mwc-list>
+        ${map(this.dashboards, (dash: string, index: number) => {
+          if (index !== 0) {
+            return html`
+              <li divider role="separator"></li>
+              <mwc-list-item>${dash}</mwc-list-item>
+            `;
+          }
+          return html`<mwc-list-item>${dash}</mwc-list-item>`;
+        })}
+      </mwc-list>
+      <mwc-button raised @click="${this.getDashboards}">Call API</mwc-button>
     `;
   }
 
   getDashboards() {
-    const result: Array<string> = [];
-    for (let i = 0; i < 100; i += 1) {
-      result.push('Foo');
-    }
-    this.dashboards = result;
+    this.dashboards = ['Loading...'];
 
     fetch('http://localhost:8080/api/v1/dashboards').then(async response => {
-      const json = await response.json();
-      console.log(json);
-      const resp = ListDashboardResponse.fromJson(json);
-      console.log(resp);
+      const resp = ListDashboardResponse.fromJson(await response.json());
 
       this.dashboards = [];
 
@@ -46,7 +49,6 @@ export class ExampleApp extends LitElement {
       min-height: 100vh;
       display: flex;
       flex-direction: column;
-      align-items: center;
       justify-content: flex-start;
       font-size: calc(10px + 2vmin);
       color: #1a2b42;
@@ -54,33 +56,6 @@ export class ExampleApp extends LitElement {
       margin: 0 auto;
       text-align: center;
       background-color: var(--example-app-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
     }
   `;
 }
