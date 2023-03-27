46ff5c9ebf7c14d51a537bce9aa1b7402050b5c7
Michelle Shepardson <amerai@google.com>
2023-03-27 20:25:35 +0000

    Update npm dependencies.

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import html from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from '@rollup/plugin-terser';
 import { generateSW } from 'rollup-plugin-workbox';
 import path from 'path';
 
