e58d582e01e352d1db90199720987a4b8ff0b377
Bryan Lou <bryanlou@google.com>
2023-06-09 21:23:16 +0000

    Combine 7 commits into 1
    Create failing tests dropdown component in dashboard tab summary
    
    Create failing tests dropdown component in dashboard tab summary
    
    Create failing tests dropdown component in dashboard tab summary
    
    Fixed some stuff
    
    Update element and file name for failures summary
    
    Embed failures summary in dashboard tab
    
    Fix failures summary class name

--- web/src/testgrid-dashboard-summary.ts
+++ web/src/testgrid-dashboard-summary.ts
@@ -3,7 +3,7 @@ import { LitElement, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { map } from 'lit/directives/map.js';
 import { Timestamp } from './gen/google/protobuf/timestamp.js';
-import { ListTabSummariesResponse, TabSummary } from './gen/pb/api/v1/data.js';
+import { FailuresSummary, ListTabSummariesResponse, TabSummary } from './gen/pb/api/v1/data.js';
 import './tab-summary.js';
 
 export interface TabSummaryInfo {
@@ -15,8 +15,24 @@ export interface TabSummaryInfo {
   lastRunTimestamp: string;
   latestGreenBuild: string;
   dashboardName: string;
+  failuresSummary?: FailuresSummaryInfo;
 }
 
+interface FailuresSummaryInfo {
+  topFailingTests: FailingTestInfo[];
+  failureStats: FailureStats;
+}
+
+interface FailingTestInfo {
+  displayName: string;
+  failCount: number;
+  passTimestamp: string;
+  failTimestamp: string;
+}
+
+interface FailureStats {
+  numFailingTests: number;
+}
 // TODO: define in a shared file (dashboard group also uses this)
 export const TabStatusIcon = new Map<string, string>([
   ['PASSING', 'done'],
@@ -42,6 +58,24 @@ function convertResponse(ts: TabSummary) {
     latestGreenBuild: ts.latestPassingBuild,
     dashboardName: ts.dashboardName,
   };
+  if (ts.failuresSummary !== undefined) {
+    tsi.failuresSummary = {} as FailuresSummaryInfo
+    const failureStats: FailureStats = {
+      numFailingTests: ts.failuresSummary!.failureStats!.numFailingTests,
+    }
+    tsi.failuresSummary!.failureStats = failureStats
+
+    tsi.failuresSummary!.topFailingTests = [];
+    ts.failuresSummary?.topFailingTests.forEach( (test, i) => {
+    const failingTest: FailingTestInfo = {
+      displayName: test.displayName,
+      failCount: test.failCount,
+      passTimestamp: Timestamp.toDate(test.passTimestamp!).toISOString(),
+      failTimestamp: Timestamp.toDate(test.failTimestamp!).toISOString(),
+    }
+    tsi.failuresSummary!.topFailingTests.push(failingTest)
+    });
+  }
   return tsi;
 }
 
